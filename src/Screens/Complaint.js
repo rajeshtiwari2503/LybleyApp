63f04b7a0ffe00189a2fdebd8db582e5f111d733
ManzarBilal <manzarbilal@gmail.com>
2023-09-16 11:52:21 +0530

    fds

---
+++ src/Screens/Complaint.js
@@ -0,0 +1,12 @@
+import { View,Text } from 'native-base'
+import React from 'react'
+
+const Complaint = () => {
+  return (
+    <View>
+        <Text>Complaint</Text>
+    </View>
+  )
+}
+
+export default Complaint;
